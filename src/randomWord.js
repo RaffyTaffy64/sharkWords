fa3552e1d272930b75cb38539f40f7f0e61a6ff7
Rafael Raeder <raf.raeder@gmail.com>
2024-08-12 14:32:17 -0600

    new sharkwords project

---
+++ src/randomWord.js
@@ -0,0 +1,18 @@
+const words = [
+  'strawberry',
+  'orange',
+  'apple',
+  'banana',
+  'pineapple',
+  'kiwi',
+  'peach',
+  'pecan',
+  'eggplant',
+  'durian',
+  'peanut',
+  'chocolate',
+];
+
+function getRandomWord() {}
+
+export default geetRandomWord
